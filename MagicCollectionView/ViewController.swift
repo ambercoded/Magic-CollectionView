38692e9797eac98a7804eb0dd2daefb34c27b8d5
Adrian <mailforadrian@me.com>
2021-02-20 05:41:44 +0100

    Initial Commit

---
+++ MagicCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicCollectionView
+//
+//  Created by Adrian on 20.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
